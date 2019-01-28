7127e6065683abf424744d923c9a90f40910fb80
Donald Linton <lintond@gmail.com>
2019-01-28 16:55:06 -0500

    FMP and FMF added

--- Python/src/Filtering.py
+++ Python/src/Filtering.py
@@ -3,6 +3,7 @@ Created on Jan 24, 2019
 
 @author: D. F. Linton, Blue Lightning Development, LLC
 '''
+from math import sin
 '''
 pip install numpy_ringbuffer
 pip install runstats
@@ -25,42 +26,77 @@ from runstats import Statistics
 from numpy_ringbuffer import RingBuffer
 
 '''
-    Pade' expanded state transition matrix of order m [RMKdR(7)]
-        P(d)_i,j = (d^(j-i))/(j-i)! where 0 <= i <= j <= m elsewhere zero
 '''
 def stateTransitionMatrix(N, dt):
+    
+    '''
+    Return a Pade' expanded state transition matrix of order m [RMKdR(7)]
+        P(d)_i,j = (d^(j-i))/(j-i)! where 0 <= i <= j <= m elsewhere zero
+    
+    :param N: return matrix is (N,N)
+    :param dt: time step
+    '''
     B = (diag(ones([N-1]),k=1))
     return expm(dt*B)
 
-def editorDefault(pBase, t, y, e):
-    return False
+class EditorNone : 
+    def __init__(self, filterBase): 
+        self.filter = filterBase
+    
+    def isGoodObservation(self, t, y, e):
+        return True
 
 class FilterBase :
-    def __init__(self):
+    def __init__(self, editingWindow=25):
         self.t0 = None
         self.t = None
         self.Z = None
-        self.n = None
-        self.E = None
-        self.edit = editorDefault
+        self.n = 0
+        self.E = zeros([editingWindow])
+        self.editor = EditorNone(self)
 
-    def initialize(self, t0, Z0, editingWindow=25):
+    def restart(self, t0, Z0):
         self.t0 = t0
         self.t = t0
-        self.Z = Z0 # normalize initial state vector
-        self.E = zeros([editingWindow])
+        self.Z = Z0
         self.n = 0
+        self.E = 0*self.E
         
-    def setEditor(self, editor):
-        self.edit = editor
+    def setEditor(self, editor, editingWindow=25):
+        self.editor = editor
+        if (editingWindow > 0) :
+            self.E = zeros([editingWindow])
+        else :
+            self.E = []
         
-    def updateGOF(self, e):
-        self.E[self.n % len(self.E)] = e
+    def updateGoodnessOfFit(self, e):
+        if (len(self.E) > 0) :
+            self.E[self.n % len(self.E)] = e
         self.n += 1
         
-    def getGOF(self):
-        m = min(len(self.E), self.n)
-        return var(self.E[0:m])
+    def getGoodnessOfFit(self):
+        if (len(self.E) > 0) :
+            m = min(len(self.E), self.n)
+            return var(self.E[0:m])
+        else :
+            return sys.float_info.max
+
+    def getBiasOfFit(self):
+        if (len(self.E) > 0) :
+            m = min(len(self.E), self.n)
+            return average(self.E[0:m])
+        else :
+            return sys.float_info.max
+        
+    # virtual methods   
+    def getTime(self):
+        raise NotImplementedError()
+    
+    def getState(self, t):
+        raise NotImplementedError()
+
+    def add(self, t, y):    
+        raise NotImplementedError()
     
 class ReynekeMorrisonFilterBase(FilterBase) :
     def __init__(self, order) :
@@ -73,54 +109,65 @@ class ReynekeMorrisonFilterBase(FilterBase) :
         # diagonal matrix D implemented as vector using element-wise operations
         self.D = None   # state denormalization vector D(tau) = [tau^-0, tau^-1,...tau^-order]
         
-    def initialize(self, t0, Z0, tau, editingWindow=25):
-        if (len(Z0) < self.order+1) :
-            raise ValueError("Z0 must be a vector of at least %d elements" % (self.order+1))
+    def setTau(self, tau):
         self.tau = tau
         self.D = array((self.tau*ones([self.order+1]))**(range(0,self.order+1)))
-        FilterBase.initialize(self, t0, self.D * Z0[0:self.order+1], editingWindow)
+        
+    def initialize(self, t0, Z0, tau):
+        if (len(Z0) < self.order+1) :
+            raise ValueError("Z0 must be a vector of at least %d elements" % (self.order+1))
+        self.setTau(tau)
+        FilterBase.restart(self, t0, self.D * Z0[0:self.order+1])
+        
+    def restart(self, t, Z):
+        FilterBase.restart(self, t, self.D * Z[0:self.order+1])
     
-    def predict(self, dtau):
+    def _predict(self, dtau):
         P = stateTransitionMatrix(self.order+1, dtau)
         return P @ self.Z
     
-    def normalizeTime(self, t):
+    def _normalizeTime(self, t):
         return (t - self.t0)/self.tau
     
-    def normalizeDeltaTime(self, dt):
+    def _normalizeDeltaTime(self, dt):
         return dt / self.tau
     
-    def denormalizeTime(self, n):
+    def _denormalizeTime(self, n):
         return self.t0 + n * self.tau
     
-    def denormalizeDeltaTime(self, dtau):
+    def _denormalizeDeltaTime(self, dtau):
         return dtau * self.tau
     
-    def denormalizeState(self, Z):
+    def _denormalizeState(self, Z):
         return Z / self.D
     
-    def getGOF(self):
+    def getGoodnessOfFit(self):
         if (self.n <= self.order+1) :
             return sys.float_info.max  # with too few samples prefer lower order
-        return FilterBase.getGOF(self)
+        return FilterBase.getGoodnessOfFit(self)
     
-    def getState(self, t):
-        if (t == self.t) :
-            return self.denormalizeState(self.Z)
-        else :
-            Z = stateTransitionMatrix(self.order+1, t-self.t) @ self.Z
-            return self.denormalizeState(Z)
-
     def getTime(self):
         return self.t
     
+    def getState(self, t):
+        if (t == self.t) :
+            return self._denormalizeState(self.Z)
+        else :
+            Z = stateTransitionMatrix(self.order+1, t-self.t) @ self.Z
+            return self._denormalizeState(Z)
+
     def add(self, t, y):
+        if (self.t == None) :
+            self.t0 = t
+            self.t = t
+            self.Z = zeros([self.order+1])
+            self.Z[0] = y
         dt = t - self.t
-        dtau = self.normalizeDeltaTime(dt)
+        dtau = self._normalizeDeltaTime(dt)
         Zstar = stateTransitionMatrix(self.order+1, dtau) @ self.Z
         e = y - Zstar[0]
-        self.updateGOF(e)
-        if(not self.edit(self, t, y, e)) :
+        self.updateGoodnessOfFit(e)
+        if(self.editor.isGoodObservation(t, y, e)) :
             gamma = self.gamma(self.gammaParameter(t, dtau))
             self.Z = Zstar + gamma * e
             self.t = t
@@ -137,20 +184,9 @@ class EMPBase(ReynekeMorrisonFilterBase):
         ReynekeMorrisonFilterBase.__init__(self, order)
         
     def gammaParameter(self, t, dtau):
-        return self.normalizeTime(t)
+        return self._normalizeTime(t)
         
-#     def add(self, t, y):
-#         dt = t - self.t
-#         dtau = self.normalizeDeltaTime(dt)
-#         Zstar = stateTransitionMatrix(self.order+1, dtau) @ self.Z
-#         e = y - Zstar[0]
-#         self.updateGOF(e)
-#         n = self.normalizeTime(t)
-#         if(not self.edit(self, t, y, e)) :
-#             gamma = self.gamma(n)
-#             self.Z = Zstar + gamma * e
-#             self.t = t
-        
+
 class FMPBase(ReynekeMorrisonFilterBase) :
     def __init__(self, order=1, theta=0.90) :
         ReynekeMorrisonFilterBase.__init__(self, order)
@@ -159,18 +195,7 @@ class FMPBase(ReynekeMorrisonFilterBase) :
     def gammaParameter(self, t, dtau):
         return self.theta ** abs(dtau)
         
-#     def add(self, t, y):
-#         return ReynekeMorrisonFilterBase.add(self, t, y, )
-#         dt = t - self.t
-#         dtau = self.normalizeDeltaTime(dt)
-#         Zstar = stateTransitionMatrix(self.order+1, dtau) @ self.Z
-#         e = y - Zstar[0]
-#         n = self.theta ** abs(dtau)
-#         if(not self.edit(self, t, y, e)) :
-#             gamma = self.gamma(n)
-#             self.Z = Zstar + gamma * e
-#             self.t = t
-    
+            
 class EMP0(EMPBase) :
     def __init__(self) :
         EMPBase.__init__(self, 0)
@@ -265,6 +290,9 @@ class EMP() :
     def initialize(self, t0, Z0, tau):
         self.filter.initialize(t0, Z0, tau)
         
+    def restart(self, t0, Z0):
+        self.filter.restart(t0, Z0)
+        
     def setEditor(self, editor):
         self.filter.setEditor(editor)      
         
@@ -274,8 +302,17 @@ class EMP() :
     def add(self, t, y):
         self.filter.add(t, y)
         
+    def getTime(self):
+        return self.filter.getTime()
+        
     def getState(self, t):
         return self.filter.getState(t)
+
+    def getBiasOfFit(self):
+        return self.filter.getBiasOfFit()
+    
+    def getGoodnessOfFit(self):
+        return self.filter.getGoodnessOfFit()
 
         
 class EMPSet():        
@@ -294,6 +331,12 @@ class EMPSet():
         for emp in self.emps :
             emp.initialize(t0, Z0[0:emp.order+1], tau)
         
+    def restart(self, t0, Z0):
+        self.current = 0
+        self.gofs = zeros([self.order+1])
+        for emp in self.emps :
+            emp.filter.restart(t0, Z0)
+        
     def setEditor(self, editor):
         for emp in self.emps :
             emp.setEditor(editor)       
@@ -304,16 +347,26 @@ class EMPSet():
     def add(self, t, y):
         for emp in self.emps :
             emp.add(t, y)
-            gof = emp.getGOF()
+            gof = emp.getGoodnessOfFit()
             self.gofs[emp.order] = gof
         j = argmin(self.gofs)
-        if (self.gofs[j] < 0.90*self.gofs[self.current]) :
-            print("%d %10.3g Switch from %d (%10.3g) to %d (%10.3g)" % (self.order, t, self.current, self.gofs[self.current], j, self.gofs[j]))
+        if (j > self.current and self.gofs[j] < 0.90*self.gofs[self.current]) :
+            print("%d %10.3g Switch from %d (%10.3g) to %d (%10.3g) %10.1f" % 
+                  (self.order, t, self.current, self.gofs[self.current], j, self.gofs[j], self.emps[self.current].nSwitch(0.9)))
             self.current = j
+        
+    def getTime(self):
+        return self.emps[self.current].getTime()
         
     def getState(self, t):
         return self.emps[self.current].getState(t)
 
+    def getBiasOfFit(self):
+        return self.emps[self.current].getBiasOfFit()
+    
+    def getGoodnessOfFit(self):
+        return self.emps[self.current].getGoodnessOfFit()
+    
         
 class FMP0(FMPBase):    
     def __init__(self, theta=0.9) :
@@ -372,24 +425,118 @@ class FMP5(FMPBase):
                       (4*3*2*1)*1.0/8.0*(1-t)**5*(1+t),
                       (5*4*3*2*1)*1.0/120.0*(1-t)**6 ])
         
+    def nSwitch(self):
+        return 7.7478/(1.0-self.theta)
+ 
+    
+class FMP() :
+    fmps = [FMP0, FMP1, FMP2, FMP3, FMP4, FMP5]
+    
+    def __init__(self, order) :
+        self.filter = self.fmps[order]()
+        
+    def initialize(self, t0, Z0, tau):
+        self.filter.initialize(t0, Z0, tau)
+        
+    def restart(self, t0, Z0):
+        self.filter.restart(t0, Z0)
+        
+    def setEditor(self, editor):
+        self.filter.setEditor(editor)      
+        
+    def add(self, t, y):
+        self.filter.add(t, y)
+        
+    def getTime(self):
+        return self.filter.getTime()
+        
+    def getState(self, t):
+        return self.filter.getState(t)
+
+    def getBiasOfFit(self):
+        return self.filter.getBiasOfFit()
+    
+    def getGoodnessOfFit(self):
+        return self.filter.getGoodnessOfFit()
+
+    
 class FixedMemoryFilter(FilterBase) :
     def __init__(self, length=51) :
         FilterBase.__init__(self)
+        self.t = None
+        self.Z = []
+        self.tRing = RingBuffer(capacity=length, dtype=numpy.double)
+        self.yRing = RingBuffer(capacity=length, dtype=numpy.double)
         
+    def getTime(self):
+        return self.t
+    
+    def getState(self, t):
+        if (t == self.t) :
+            return self.Z
+        else :
+            Z = stateTransitionMatrix(2+1, t-self.getTime()) @ self.Z
+            return Z
+
+    def add(self, t, y):    
+        self.tRing.appendleft(t)
+        self.yRing.appendleft(y)
+        if (self.tRing.is_full) :
+            T = array(self.tRing)
+            Y = array(self.yRing)
+            L = len(T)
+            TntTn = zeros([3,3]) # Tn' Tn
+            TntYn = zeros([3,1]) # Tn' Yn
+            for k in range(0,L) :
+                dt = T[0] - T[k]
+                dt2 = dt*dt
+                dt3 = dt2*dt
+                dt4 = dt2*dt2
+                TntTn[0,0] += 1 
+                TntTn[0,1] += -dt 
+                TntTn[0,2] += +dt2
+                TntTn[1,0] += -dt 
+                TntTn[1,1] += +dt2 
+                TntTn[1,2] += -dt3
+                TntTn[2,0] += +dt2
+                TntTn[2,1] += -dt3 
+                TntTn[2,2] += +dt4
+                TntYn[0,0] += Y[k]
+                TntYn[1,0] += -dt*Y[k]
+                TntYn[2,0] += dt2*Y[k]
+            Ystar = inv(TntTn) @ TntYn
+            self.t = T[0]
+            self.Z = array([1, 1, 2*1]) * Ystar[:,0]
+            self.updateGoodnessOfFit(y - self.Z[0])
+
 
 def generateTestData(order, N, t0, Y0, dt, bias=0.0, sigma=1.0):
-    truth = zeros([N,order+1])
-    observations = bias + sigma*randn(N,1)
-    times = zeros([N,1])
-    S = stateTransitionMatrix(order+1, dt)
-    t = t0 + dt
-    Y = Y0
-    for i in range(0,N) :
-        Y = S @ Y
-        observations[i] += Y[0]
-        times[i] = t
-        truth[i,:] = Y[:]
-        t = t+dt
+    if (order >= 0) :
+        truth = zeros([N,order+1])
+        observations = bias + sigma*randn(N,1)
+        times = zeros([N,1])
+        S = stateTransitionMatrix(order+1, dt)
+        t = t0 + dt
+        Y = Y0
+        for i in range(0,N) :
+            Y = S @ Y
+            observations[i] += Y[0]
+            times[i] = t
+            truth[i,:] = Y[:]
+            t = t+dt
+    else :
+        order = -order
+        truth = zeros([N,order+1])
+        observations = bias + sigma*randn(N,1)
+        times = zeros([N,1])
+        t = t0 + dt
+        Y = Y0
+        for i in range(0,N) :
+            Y[0] = sin(t)
+            observations[i] += Y[0]
+            times[i] = t
+            truth[i,:] = Y[:]
+            t = t+dt
     return (times, truth, observations)
 
        
@@ -419,39 +566,30 @@ def fixedMemoryFilter(t, y):
     return array([1, 1, 2*1]) * Ystar[:,0]
      
 def testFixedMemoryFilter() :
-    N = 21
+    N = 101
     y0 = 100.0
     y1 = 10.0
     y2 = 5.0
-    Y0 = array([y0, y1, y2]);
+    Y0 = array([y0, y1, y2, 2.0, 1.0, 0.5]);
     tau = 0.1
-    (times, truth, observations) = generateTestData(2, N, 0.0, Y0, tau, 0.0, 0.0)
-    tRing = RingBuffer(capacity=10, dtype=numpy.double)
-    yRing = RingBuffer(capacity=10, dtype=numpy.double)
-    for i in range(0,N) :
-        tRing.appendleft(times[i])
-        yRing.appendleft(observations[i])
-        if (i > 10) :
-            Y = fixedMemoryFilter( array(tRing), array(yRing) )
-            print("%10.4g %10.4g %10.4g %10.4g" % (Y[0],Y[1],Y[2],truth[i,0]))
+    (times, truth, observations) = generateTestData(-5, N, 0.0, Y0, tau, 0.0, 10.0)
     
-def testFMP():
-    N = 21
-    y0 = 100.0
-    y1 = 10.0
-    y2 = 5.0
-    Y0 = array([y0, y1, y2, 2.5, 1, 0.5])
-    tau = 0.1
-    (times, truth, observations) = generateTestData(5, N, 0.0, Y0, tau, 0.0, 0.0)
-    fmp = FMP5(0.9)
-    fmp.initialize(0.0, Y0, tau)
+    fmf = FixedMemoryFilter(11)
     for i in range(0,N) :
-        fmp.add(times[i][0], truth[i,0])
-        print(fmp.getState(times[i]))
-              
+        fmf.add(times[i], observations[i])
+        if (fmf.getTime() != None) :
+            print(i, fmf.getTime(), fmf.getState(fmf.getTime()), truth[i,0:3], fmf.getGoodnessOfFit(), fmf.getBiasOfFit() )
+#     tRing = RingBuffer(capacity=10, dtype=numpy.double)
+#     yRing = RingBuffer(capacity=10, dtype=numpy.double)
+#     for i in range(0,N) :
+#         tRing.appendleft(times[i])
+#         yRing.appendleft(observations[i])
+#         if (i > 10) :
+#             Y = fixedMemoryFilter( array(tRing), array(yRing) )
+#             print("%10.4g %10.4g %10.4g %10.4g" % (Y[0],Y[1],Y[2],truth[i,0]))
+    
 if __name__ == '__main__':
     pass
-    testFMP()
     testFixedMemoryFilter()
 #     A = array([[0,1,0],[0,0,1], [0,0,0]])
 #     print(expm(0.1*A))
@@ -460,8 +598,8 @@ if __name__ == '__main__':
 #         print( stateTransitionMatrix(n, 0.1) )
 #     base = ReynekeMorrisonFilterBase(2)
 #     base.initialize(0.0, (array([100.0, 10.0, 1.0])), 0.1)
-#     print(base.normalizeDeltaTime(0.1))
-#     P = stateTransitionMatrix(2+1, base.normalizeDeltaTime(0.1))
+#     print(base._normalizeDeltaTime(0.1))
+#     P = stateTransitionMatrix(2+1, base._normalizeDeltaTime(0.1))
 #     print(P)
 #     print(base.D)
 #     X = array([100.0, 10.0, 1.0])
--- Python/test/Filtering_test.py
+++ Python/test/Filtering_test.py
@@ -5,25 +5,40 @@ Created on Jan 24, 2019
 '''
 import unittest
 
-from numpy import array, transpose, zeros, argmin
+from numpy import array, transpose, zeros, argmin, array2string
 from numpy.linalg import norm
 from numpy.random import randn
-from Filtering import ReynekeMorrisonFilterBase, stateTransitionMatrix, EMP0, EMP1, EMP2, EMP3, EMP4, EMP5, EMP, EMPSet
+from Filtering import ReynekeMorrisonFilterBase, stateTransitionMatrix, EMP0, EMP1, EMP2, EMP3, EMP4, EMP5, EMP, EMPSet,FMP0, FMP1, FMP2, FMP3, FMP4, FMP5 #, FMP, FMPSet
 from runstats import Statistics
+from math import sin
 
 def generateTestData(order, N, t0, Y0, dt, bias=0.0, sigma=1.0):
-    truth = zeros([N,order+1])
-    observations = bias + sigma*randn(N,1)
-    times = zeros([N,1])
-    S = stateTransitionMatrix(order+1, dt)
-    t = t0 + dt
-    Y = Y0
-    for i in range(0,N) :
-        Y = S @ Y
-        observations[i] += Y[0]
-        times[i] = t
-        truth[i,:] = Y[:]
-        t = t+dt
+    if (order >= 0) :
+        truth = zeros([N,order+1])
+        observations = bias + sigma*randn(N,1)
+        times = zeros([N,1])
+        S = stateTransitionMatrix(order+1, dt)
+        t = t0 + dt
+        Y = Y0
+        for i in range(0,N) :
+            Y = S @ Y
+            observations[i] += Y[0]
+            times[i] = t
+            truth[i,:] = Y[:]
+            t = t+dt
+    else :
+        order = -order
+        truth = zeros([N,order+1])
+        observations = bias + sigma*randn(N,1)
+        times = zeros([N,1])
+        t = t0 + dt
+        Y = Y0
+        for i in range(0,N) :
+            Y[0] = sin(t)
+            observations[i] += Y[0]
+            times[i] = t
+            truth[i,:] = Y[:]
+            t = t+dt
     return (times, truth, observations)
 
 class Test(unittest.TestCase):
@@ -37,7 +52,7 @@ class Test(unittest.TestCase):
         pass
 
 
-    def testPBase(self):
+    def testReynekeMorrisonFilterBase(self):
         for order in range(0,6) :
             base = ReynekeMorrisonFilterBase(order)
             self.assertEqual(base.order, order, "order %d incorrect" % order)
@@ -53,9 +68,9 @@ class Test(unittest.TestCase):
         base.initialize(0.0, Y0, 0.1)
         self.assertEqual(len(base.Z), order+1, "state length incorrect; was %d expected %d" % (len(base.Z), order+1))
         self.assertEqual(len(base.D), order+1, "Denormalization vector length incorrect")
-        dtau = base.normalizeDeltaTime(0.1)
-        Z = base.predict(dtau)
-        Y = base.denormalizeState(Z)
+        dtau = base._normalizeDeltaTime(0.1)
+        Z = base._predict(dtau)
+        Y = base._denormalizeState(Z)
         self.assertEqual( Y[0], 100.0+0.1*10.0+0.5*0.1**2*1.0, "Bad prediction element 0")
         self.assertEqual( Y[1], 10.0+0.1*1.0, "Bad prediction element 1")
         self.assertEqual( Y[2], 1.0, "Bad prediction element 2")                  
@@ -195,6 +210,7 @@ class Test(unittest.TestCase):
 # #             self.assertLess(norm((Yf-truth[i,:])/Yf), 2.5, 'Excess noise state error')
 #         print(stats.mean(), stats.variance(), emp.VRF(N-15) )
 
+
     def testEMP(self):
         N = 100
         y0 = 100.0
@@ -214,42 +230,72 @@ class Test(unittest.TestCase):
                 if (i > 5) :
                     self.assertLess(norm(Yf[0]-truth[i,0]), 1e-6, 
                                 'Noiseless state error (%d, %d)' % (order, i))
+        order = 5
+        
+        (times, truth, observations) = generateTestData(-5, N, 0.0, Y0, 0.1)
+        emp = EMPSet(5)
+        emp.initialize(0.0, Y0, 0.1)
+        for i in range(0,N) :
+            emp.add(times[i][0], truth[i,0])
+            Yf = emp.getState(times[i][0])
+            r = array2string(Yf, formatter={'float_kind':lambda y: "%10.4g" % y})
+            print("%5d %10.4g %s %10.4g" % (i, times[i][0], r, Yf[0]-truth[i,0]))
 
         
 
-    def testGoodnessOfFit(self):
-        N = 300
+#     def testGoodnessOfFit(self):
+#         N = 300
+#         y0 = 100.0
+#         y1 = 10.0
+#         y2 = 1.0
+#         Y0 = array([y0, y1, y2, 0.5, 0.25, -0.125]);
+#         tau = 0.1
+#         (times, truth, observations) = generateTestData(5, N, 0.0, Y0, tau)
+#         print(times[0], truth[0,:] )
+#         print(times[-1], truth[-1,:] )
+#          
+#         empSet = [EMP0(), EMP1(),EMP2(), EMP3(), EMP4(), EMP5()]
+#         for i in range(0, len(empSet)):
+#             Y0 = zeros([i+1])
+#             Y0[0] = y0
+#             empSet[i].initialize(0.0, Y0, 0.1)
+#             
+#         currentEMP = 0
+#         gofs = zeros([len(empSet)])
+#         for i in range(0,N) :
+#             report = "%5d: " % (i)
+#             for e in empSet :
+#                 e.add(times[i][0], observations[i])
+#                 gof = e.getGOF()
+#                 report += " %10.3g" % (gof)
+#                 gofs[e.order] = gof
+#             j = argmin(gofs)
+#             if (gofs[j] < 0.90*gofs[currentEMP]) :
+#                 print("%5d Switch from %d (%10.3g) to %d (%10.3g)" % (i, currentEMP, gofs[currentEMP], j, gofs[j]))
+#                 currentEMP = j
+# #             print(report)
+#         print(report)
+#         print( empSet[currentEMP].getState(times[-1][0]) )
+        
+    def testFMP(self):
+        N = 100
         y0 = 100.0
         y1 = 10.0
-        y2 = 1.0
-        Y0 = array([y0, y1, y2, 0.5, 0.25, -0.125]);
+        y2 = 5.0
+        Y0 = array([y0, y1, y2, 2.5, 1, 0.5])
         tau = 0.1
-        (times, truth, observations) = generateTestData(5, N, 0.0, Y0, tau)
-        print(times[0], truth[0,:] )
-        print(times[-1], truth[-1,:] )
-         
-        empSet = [EMP0(), EMP1(),EMP2(), EMP3(), EMP4(), EMP5()]
-        for i in range(0, len(empSet)):
-            Y0 = zeros([i+1])
-            Y0[0] = y0
-            empSet[i].initialize(0.0, Y0, 0.1)
-            
-        currentEMP = 0
-        gofs = zeros([len(empSet)])
+        (times, truth, observations) = generateTestData(5, N, 0.0, Y0, tau, 0.0, 1.0)
+        fmp = FMP5(0.9)
+        print("FMP Ns %10.1f" % fmp.nSwitch())
+#         fmp.initialize(0.0, Y0, tau)
+        fmp.setTau(tau)
         for i in range(0,N) :
-            report = "%5d: " % (i)
-            for e in empSet :
-                e.add(times[i][0], observations[i])
-                gof = e.getGOF()
-                report += " %10.3g" % (gof)
-                gofs[e.order] = gof
-            j = argmin(gofs)
-            if (gofs[j] < 0.90*gofs[currentEMP]) :
-                print("%5d Switch from %d (%10.3g) to %d (%10.3g)" % (i, currentEMP, gofs[currentEMP], j, gofs[j]))
-                currentEMP = j
-#             print(report)
-        print(report)
-        print( empSet[currentEMP].getState(times[-1][0]) )
+            fmp.add(times[i][0], observations[i,0])
+            Z = fmp.getState(times[i])
+            r = array2string((Z-truth[i,:])/truth[i,:], formatter={'float_kind':lambda y: "%10.4g" % y})
+
+            print("%10.4g %10.4g %s %10.4g" % (times[i][0], truth[i,0], r, Z[0] - truth[i,0]))
+                  
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
